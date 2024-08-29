f1969141c364804925914e670c48dcf9c8e51531
Dustin Parker <pagesbydustin@gmail.com>
2024-08-28 18:29:12 -0700

    Added accordian to rules

--- src/assets/componenents/DescriptionComponent.jsx
+++ src/assets/componenents/DescriptionComponent.jsx
@@ -1,5 +1,12 @@
 import { useState } from "react";
-import { Button, Col, Container, Offcanvas, Row } from "react-bootstrap";
+import {
+  Accordion,
+  Button,
+  Col,
+  Container,
+  Offcanvas,
+  Row,
+} from "react-bootstrap";
 import DataFile from "../data/RulesAndSettingsData.json";
 function DescriptionComponent() {
   const [show, setShow] = useState(false);
@@ -16,77 +23,67 @@ function DescriptionComponent() {
       <Button variant="dark" onClick={handleOpen}>
         Open rules and settings desciption
       </Button>
-      <Offcanvas id="OCDescription" className="w-50" show={show} onHide={handleClose}>
+      <Offcanvas
+        id="OCDescription"
+        className="w-50"
+        show={show}
+        onHide={handleClose}
+      >
         <Offcanvas.Header closeButton>
           <Offcanvas.Title>{DataFile.GameName}</Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
           <h3>Rules</h3>
-
-          <Container fluid>
-            <Row fluid className="fw-bold">
-              Dice
-              <hr />
-            </Row>
-            <Row fluid>{DataFile.Rules["Dice"]}</Row>
-          </Container>
           <br />
           <Container fluid>
-            <Row fluid className="fw-bold">
-              Rolling The Dice
-              <hr />
-            </Row>
-            <Row fluid>{DataFile.Rules["Rolling the Dice"]}</Row>
+            <Accordion>
+              <Accordion.Item eventKey={0}>
+                <Accordion.Header>Dice</Accordion.Header>
+                <Accordion.Body>{DataFile.Rules["Dice"]}</Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey={1}>
+                <Accordion.Header>Rolling The Dice</Accordion.Header>
+                <Accordion.Body>
+                  {DataFile.Rules["Rolling the Dice"]}
+                </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey={2}>
+                <Accordion.Header>Winning</Accordion.Header>
+                <Accordion.Body>{DataFile.Rules["How to win"]}</Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
           </Container>
           <br />
-          <Container fluid>
-            <Row fluid className="fw-bold">
-              Winning
-              <hr />
-            </Row>
-            <Row fluid>{DataFile.Rules["How to win"]}</Row>
-          </Container>
-          <br />
-
           <h3>Settings</h3>
-          <Container fluid>
-            <Row fluid className="fw-bold">
-              GamerTag
-              <hr />
-            </Row>
-            <Row fluid>{DataFile.Settings.GamerTag}</Row>
-          </Container>
           <br />
           <Container fluid>
-            <Row fluid className="fw-bold">
-              Persistant Score
-              <hr />
-            </Row>
-            <Row fluid>{DataFile.Settings["Persistant Score"]}</Row>
-          </Container>
-          <br />
-          <Container fluid>
-            <Row fluid className="fw-bold">
-              Timer
-              <hr />
-            </Row>
-            <Row fluid>{DataFile.Settings.Timer}</Row>
-          </Container>
-          <br />
-          <Container fluid>
-            <Row fluid className="fw-bold">
-              Rounds
-              <hr />
-            </Row>
-            <Row fluid>{DataFile.Settings.Rounds}</Row>
-          </Container>
-          <br />
-          <Container fluid>
-            <Row fluid className="fw-bold">
-              Number of Players
-              <hr />
-            </Row>
-            <Row fluid>{DataFile.Settings["Number of Players"]}</Row>
+            <Accordion>
+              <Accordion.Item eventKey={0}>
+                <Accordion.Header>Gamer Tag</Accordion.Header>
+                <Accordion.Body>{DataFile.Settings.GamerTag}</Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey={1}>
+                <Accordion.Header>Persistant Score</Accordion.Header>
+                <Accordion.Body>
+                  {DataFile.Settings["Persistant Score"]}
+                </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey={2}>
+                <Accordion.Header>Timer</Accordion.Header>
+                <Accordion.Body>{DataFile.Settings.Timer}</Accordion.Body>
+              </Accordion.Item>
+
+              <Accordion.Item eventKey={3}>
+                <Accordion.Header>Rounds</Accordion.Header>
+                <Accordion.Body>{DataFile.Settings.Rounds}</Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey={4}>
+                <Accordion.Header>Number of Players</Accordion.Header>
+                <Accordion.Body>
+                  {DataFile.Settings["Number of Players"]}
+                </Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
           </Container>
         </Offcanvas.Body>
       </Offcanvas>
