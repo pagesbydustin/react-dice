497149b6203285a4465cd351f10547a3f349fb12
Dustin Parker <pagesbydustin@gmail.com>
2024-09-13 22:20:18 -0700

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -134,29 +134,6 @@ function App() {
         </Container>
       ) : (
         <>
-          {/* <Container
-            id="diceTableHeading"
-            hidden={gameStarted ? true : false}
-            className="text-center mb-1 mt-1 p-2 bg-black rounded-5 bg-opacity-75"
-            width={"1280px"}
-          >
-            <h1 className="text-white">Roll The Die</h1>
-            <p>Drag to the right and let go to roll the die!</p>
-          </Container>
-          <Container
-            hidden={gameStarted ? true : false}
-            className="container-fluid bg-black bg-opacity-75 rounded-5 p-3"
-            id="diceTable"
-          >
-            <animated.img
-              style={props}
-              draggable={true}
-              src={`/dice/die${powerDieNumber}.svg`}
-              className="die"
-              alt="die logo ${powerDieNumber}"
-              onDrag={handleDiceRoll}
-            />
-          </Container> */}
           <DieTableComponent
             dice={
               <animated.img
